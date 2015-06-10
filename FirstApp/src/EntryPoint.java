9a8f200b466330a6a823c5e6f6415cbddd3066a3
Shravan251 <shravan.niranjan@live.com>
2015-06-10 22:36:35 +0530

    Program number pattern

---
+++ FirstApp/src/EntryPoint.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class EntryPoint {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number0");
+		int n=sc.nextInt();
+		System.out.println(n);
+	}
+
+}
